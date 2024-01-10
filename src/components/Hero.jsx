532262c4c9f54c95c662b28d360bd171303d67cc
ajanaku <mykdahunsi@gmail.com>
2024-01-10 18:44:05 +0100

    Briefly first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="Briefly-logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://")}
+          className="git_btn"
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with Briefly
+        <br className="sm-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Briefly. An open-source article summarizer
+        that turns lengthy articles into clear, concise summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
