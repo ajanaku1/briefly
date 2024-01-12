1d79e0aa8d3605adedfdf4a60482c0c3c8ce13b0
ajanaku <mykdahunsi@gmail.com>
2024-01-12 20:58:45 +0100

    logo clear

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,11 @@ const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="Briefly-logo" className="w-28 object-contain" />
+        <img
+          src={logo}
+          alt="Briefly-logo"
+          className="w-28 object-contain z-50"
+        />
         <button
           type="button"
           onClick={() => window.open("https://github.com/ajanaku1/briefly.git")}
